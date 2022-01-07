e6ad53f7e1ccd3e6aed163565644d032c28f4cdb
pci-qs <cpak@qompass-solutions.com>
2022-01-07 18:31:53 +0300

    server.js changed

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { createServer } = require('http');
 const WebSocket = require('ws');
 
 const app = express();
-app.use(express.static('/public'));
+app.use(express.static(path.join(__dirname, '/public')));
 
 const server = createServer(app);
 const wss = new WebSocket.Server({ server });
